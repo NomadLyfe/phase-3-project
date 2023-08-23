d20672438e93a1abaaeae36a8950614a5efc2d20
Jeremy Garcia <jeremygarcia9400@gmail.com>
2023-08-23 12:04:37 -0400

    upgraded alembic to have games, users, game_users tables

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Integer, create_engine, Column, Text, ForeignKey, Table
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
-from constants import B_ROOK,B_KNIGHT,B_BISHOP,B_QUEEN,B_KING,B_BISHOP,B_KNIGHT,B_ROOK,B_PAWN,W_ROOK,W_KNIGHT,W_BISHOP,W_QUEEN,W_KING,W_BISHOP,W_KNIGHT,W_ROOK,W_PAWN,B_EMPTY,W_EMPTY
+from constants import board_state
 
 Base = declarative_base()
 
@@ -20,7 +20,7 @@ game_user = Table(
 class User(Base):
     __tablename__ = 'users'
 
-    id = Column(Integer(), primary_kay=True)
+    id = Column(Integer(), primary_key=True)
     username = Column(Text())
 
     games = relationship('Game', secondary=game_user, back_populates='users')
@@ -30,14 +30,7 @@ class Game(Base):
 
     id = Column(Integer(), primary_key=True)
     turn_count = Column(Integer())
-    board_state = [[B_ROOK,B_KNIGHT,B_BISHOP,B_QUEEN,B_KING,B_BISHOP,B_KNIGHT,B_ROOK],
-               [B_PAWN,B_PAWN,B_PAWN,B_PAWN,B_PAWN,B_PAWN,B_PAWN,B_PAWN],
-               [W_EMPTY,B_EMPTY,W_EMPTY,B_EMPTY,W_EMPTY,B_EMPTY,W_EMPTY,B_EMPTY],
-               [B_EMPTY,W_EMPTY,B_EMPTY,W_EMPTY,B_EMPTY,W_EMPTY,B_EMPTY,W_EMPTY],
-               [W_EMPTY,B_EMPTY,W_EMPTY,B_EMPTY,W_EMPTY,B_EMPTY,W_EMPTY,B_EMPTY],
-               [B_EMPTY,W_EMPTY,B_EMPTY,W_EMPTY,B_EMPTY,W_EMPTY,B_EMPTY,W_EMPTY],
-               [W_PAWN,W_PAWN,W_PAWN,W_PAWN,W_PAWN,W_PAWN,W_PAWN,W_PAWN],
-               [W_ROOK,W_KNIGHT,W_BISHOP,W_QUEEN,W_KING,W_BISHOP,W_KNIGHT,W_ROOK]]
+    board_state = board_state
     active_game = True
     
     users = relationship('User', secondary=game_user, back_populates='games')
