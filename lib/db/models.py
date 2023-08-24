f6fe61425d96904f8c54ba0cdf8e5c4ea1e84576
Jeremy Garcia <jeremygarcia9400@gmail.com>
2023-08-23 20:05:33 -0400

    fixed GameUser, Game, and User models

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,25 +1,25 @@
-from sqlalchemy import Integer, create_engine, Column, Text, ForeignKey, Table, MetaData
+from sqlalchemy import Integer, create_engine, Column, Text, ForeignKey, MetaData
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.associationproxy import association_proxy
 from constants import board_state
 
-convention = {
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-}
-metadata = MetaData(naming_convention=convention)
-Base = declarative_base(metadata=metadata)
+Base = declarative_base()
 
 engine = create_engine('sqlite:///chessdatabase.db')
-Session = sessionmaker(bind=Base)
+Session = sessionmaker(bind=engine)
 session = Session()
 
-game_user = Table(
-    'game_users',
-    Base.metadata,
-    Column('user_id', ForeignKey('users.id'), primary_key=True),
-    Column('game_id', ForeignKey('games.id'), primary_key=True),
-    extend_existing=True
-)
+class GameUser(Base):
+    __tablename__ = 'game_users'
+
+    id = Column(Integer(), primary_key=True)
+
+    user_id = Column(Integer(), ForeignKey('users.id'))
+    game_id = Column(Integer(), ForeignKey('games.id'))
+
+    game = relationship('Game', back_populates='game_users')
+    user = relationship('User', back_populates='game_users')
 
 class User(Base):
     __tablename__ = 'users'
@@ -27,7 +27,8 @@ class User(Base):
     id = Column(Integer(), primary_key=True)
     username = Column(Text())
 
-    games = relationship('Game', secondary=game_user, back_populates='users')
+    game_users = relationship('GameUser', back_populates='user', cascade='all, delete-orphan')
+    games = association_proxy('game_users', 'game', creator=lambda gm: GameUser(game=gm))
 
 class Game(Base):
     __tablename__ = 'games'
@@ -37,4 +38,5 @@ class Game(Base):
     board_state = board_state
     active_game = True
     
-    users = relationship('User', secondary=game_user, back_populates='games')
+    game_users = relationship('GameUser', back_populates='game', cascade='all, delete-orphan')
+    users = association_proxy('game_users', 'user', creator=lambda us: GameUser(user=us))
